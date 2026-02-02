66be052242bc12466b99f2b1d1a8dca19c79bea4
avivanguyen <aviva.nguyen@gmail.com>
2026-02-02 16:02:56 -0700

    fixed 'zero' btn to console correctly

---
+++ .history/calc_20260202154522.js
@@ -0,0 +1,109 @@
+let calculation = '';
+
+function updateDisplay() {
+  document.getElementById('display').innerText = calculation;
+}
+
+//NUMBERS BTN
+function addOne() {
+  calculation += '1';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function addTwo() {
+  calculation += '2';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function addThree() {
+  calculation += '3';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function addFour() {
+  calculation += '4';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function addFive() {
+  calculation += '5';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function addSix() {
+  calculation += '6';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function addSeven() {
+  calculation += '7';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function addEight() {
+  calculation += '8';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function addNine() {
+  calculation += '9';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function addZero() {
+  calculation += '0';
+  console.log()
+}
+
+//SYMBOLS BTN
+function plus() {
+  calculation += ' + ';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function minus() {
+  calculation += ' - ';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function multiply() {
+  calculation += ' * ';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function divide() {
+  calculation += ' / ';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function equals() {
+  calculation = eval(calculation);
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function decimal() {
+  calculation += '.';
+  console.log(`${calculation}`);
+  updateDisplay();
+}
+
+function clearCalc() {
+  calculation = '';
+  console.log('Cleared');
+  console.log(`${calculation}`);
+  updateDisplay();
+}
